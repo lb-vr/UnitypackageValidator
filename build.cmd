a144378dbbefa65389a0f085e761a3f6e3b07254
Shirao Shotaro <shirao.shotaro@gmail.com>
2020-04-18 21:22:08 +0900

    add entry python file and modified build.cmd

--- build.cmd
+++ build.cmd
@@ -1,15 +1,15 @@
 rem ツール名を指定する
-set TOOL_NAME=vemt-unitypackage-validator
+set TOOL_NAME=vemt-cket-validator
 set ICON_PATH=%~dp0favicon.ico
 set ADD_PATH=
 rem -----------------------------------------------
 rem 以下変更不要
 rem -----------------------------------------------
 set EXE_PATH=%~dp0exe
-set TOOL_PATH=%~dp0src
+set TOOL_PATH=%~dp0
 cd %EXE_PATH%
 rem pyinstallerを実行
-pyinstaller %TOOL_PATH%\main.py --hidden-import=PySide2.QtXml --onefile --clean --icon %ICON_PATH% -n %TOOL_NAME%
+pyinstaller %TOOL_PATH%\cket_main.py --hidden-import=PySide2.QtXml --onefile --clean --icon %ICON_PATH% -n %TOOL_NAME%
 rem pyinstaller %TOOL_PATH%\main.py --onefile --clean --icon %ICON_PATH% -n %TOOL_NAME%-p %ADD_PATH%;%TOOL_PATH%
 cd ..
 rem mkdir %EXE_PATH%\dist\ui
